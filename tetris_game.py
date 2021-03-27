fd1a37d4fdf3a18f4320dcda96165d09f4c31bba
Maciej Topolewski <maciej@top-project.com.pl>
2021-03-27 17:53:59 +0100

    tetr 4

--- tetris_game.py
+++ tetris_game.py
@@ -37,6 +37,7 @@ class Tetris:
         self.new_boards = []
         self.fixed_boards = []
         self.current_pos = []
+        self.pos_dict = {}
         self.new_pos = []
 
     def board_screen(self):
@@ -84,7 +85,13 @@ class Tetris:
         self.current_shape=shape
         for board in self.boards:
             print(*["*" if elem==1 else " " for elem in board], sep='')
-#            self.current_pos.append([i for i,x in enumerate(board) if x == 1])
+            self.current_pos.append([i for i, x in enumerate(board) if x == 1])
+        for pos in self.current_pos:
+            pos.remove(0)
+            pos.remove(21)
+        for i,pos in enumerate(self.current_pos[:-1]):
+            if len(pos)>0:
+                self.pos_dict[i] = pos
 
     def first_move(self):
         self.new_boards = self.boards[:-1]
@@ -93,53 +100,51 @@ class Tetris:
             if move == "d":
                 for board in self.new_boards:
                     print(*["*" if elem==1 else " " for elem in board], sep='')
-                print(self.boards)
                 print("**********////////////*********")
                 for i, board in enumerate (self.boards[:-1]):
                     if board.count(1) > 2:
-#                        self.new_boards[i] = self.board_line
                         self.new_boards[i+1] = self.boards[i]
                 for i, board in enumerate (self.new_boards):
-                    print(board)
                     if board.count(1) > 2:
                         self.new_boards[i] = self.board_line
                         break
-                    # for i, item in enumerate (board):
-                    #     if i != 0 and i != 21 and item == 1:
-                    #         self.current_boards.append(i)
+                print(self.pos_dict)
+                for k in self.pos_dict.keys():
+                    single_list = self.new_boards[k]
+                    for item in self.pos_dict.get(k):
+                        self.new_boards[k][item]=0
+                        print(k, item)
+                for k in self.pos_dict.keys():
+                    single_list = self.new_boards[k]
+                    for item in self.pos_dict.get(k):
+                        self.new_boards[k+1][item+1]=1
+                        print(k, item)
 
 
 
-                # print(self.current_pos)
-                # for pos in self.current_pos[:-1]:
-                #     for p in pos:
-                #         if p !=0 and p !=21:
-                #             p+=1
-                #             self.new_pos.append(p)
-                #
-                # print(self.new_pos)
-#                print(self.current_pos)
-                # for board in self.boards:
-                #     for b in board:
-                #         print(b)
-                #         if b != 0 and b != 21:
-                #             b += 1
+                    # self.new_boards[k][v]=0
+                    # self.new_boards[k][v+1]=1
+
+                # for i, board in self.new_boards:
+                #     if board.count(1) > 2:
+                #         for i, num in enumerate (board):
+                #             if num==1 and i !=0 and i != 21:
+                #                 board[i]=0
+                #                 board[i+1]=1
+                #                 print(board)
+
+                    # for i, b in enumerate (board):
+                    #     if board[i] != 0 and board[i] != 21:
+                    #         board[i] == 0
+                    #         board[i+1] ==1
                 self.new_boards.append([1].copy()*22)
                 for board in self.new_boards:
                     print(*["*" if elem==1 else " " for elem in board], sep='')
-
-                # m = 0
-                # for i in range(len(self.current_shape)):
-                #     sub_shape = self.current_shape[m]
-                #     n = 0
-                #     pos = position
-                #     for i in range(len(sub_shape)):
-                #         if sub_shape[n] == 1 and pos<=21:
-                #             self.boards[m+1][pos] = 1
-                #         pos+=1
-                #         n+=1
-                #     m+=1
                 continue
+
+
+
+
             elif move == "s":
                 for board in self.boards:
                     print(*["*" if elem==1 else " " for elem in board], sep='')
@@ -162,3 +167,42 @@ class Tetris:
 tetr=Tetris()
 tetr.board_screen()
 tetr.first_move()
+
+
+
+
+
+                # m = 0
+                # for i in range(len(self.current_shape)):
+                #     sub_shape = self.current_shape[m]
+                #     n = 0
+                #     pos = position
+                #     for i in range(len(sub_shape)):
+                #         if sub_shape[n] == 1 and pos<=21:
+                #             self.boards[m+1][pos] = 1
+                #         pos+=1
+                #         n+=1
+                #     m+=1
+
+
+
+                    # for i, item in enumerate (board):
+                    #     if i != 0 and i != 21 and item == 1:
+                    #         self.current_boards.append(i)
+
+
+
+                # print(self.current_pos)
+                # for pos in self.current_pos[:-1]:
+                #     for p in pos:
+                #         if p !=0 and p !=21:
+                #             p+=1
+                #             self.new_pos.append(p)
+                #
+                # print(self.new_pos)
+#                print(self.current_pos)
+                # for board in self.boards:
+                #     for b in board:
+                #         print(b)
+                #         if b != 0 and b != 21:
+                #             b += 1
